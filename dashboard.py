50ffc183d7c995de6cfeaa0e324eab10f73330e4
Veluri Bharath <veluribharath@gmail.com>
2023-02-27 13:02:37 +0530

    add plotly code

--- dashboard.py
+++ dashboard.py
@@ -1,8 +1,10 @@
 import streamlit as st
-
+import pandas as pd
 from matplotlib import pyplot as plt
 import fastf1
 import fastf1.plotting
+
+import plotly.express as px
 
 fastf1.plotting.setup_mpl()
 
@@ -17,11 +19,10 @@ lec_car_data = fast_leclerc.get_car_data()
 fast_ham = session.laps.pick_driver('HAM').pick_fastest()
 ham_car_data = fast_ham.get_car_data()
 
+ham_car_data.to_csv('_cache/ham_test_data.csv',index = False)
+
 t = lec_car_data['Time']
 vCar = lec_car_data['Speed']
-print(type(t), t)
-print(type(t[0]))
-print(type(vCar), vCar)
 
 # The rest is just plotting
 fig, ax = plt.subplots()
@@ -34,5 +35,9 @@ ax.legend()
 # plt.show()
 
 st.pyplot(fig)
+ham_car_data['Time'] = pd.to_timedelta(ham_car_data['Time'])
 
-st.line_chart(lec_car_data, x = 'Time', y = 'Speed')
+# fig = px.line(lec_car_data, x='Time', y='Speed', labels='Lec')
+fig = px.line(ham_car_data,  x='Time', y='Speed', labels='Ham')
+
+st.plotly_chart(fig)
